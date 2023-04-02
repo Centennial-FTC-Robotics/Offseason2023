3fd57e301e3cb0bda3457f64155d6f3c5a58353c
pi-person314 <jadenmengl@gmail.com>
2023-04-02 16:59:08 -0400

    actual stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Jaden/TeleOpJaden.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Jaden/TeleOpJaden.java
@@ -3,14 +3,57 @@ package org.firstinspires.ftc.teamcode.Jaden;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.Range;
 
 @TeleOp
 public class TeleOpJaden extends LinearOpMode {
 
-    private DcMotor leftMotor;
-    private DcMotor rightMotor;
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+
+    public void initialize(LinearOpMode opMode) {
+        frontLeft = opMode.hardwareMap.dcMotor.get("frontLeft");
+        frontRight = opMode.hardwareMap.dcMotor.get("frontRight");
+        backLeft = opMode.hardwareMap.dcMotor.get("backLeft");
+        backRight = opMode.hardwareMap.dcMotor.get("backRight");
+
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
     @Override
     public void runOpMode() throws InterruptedException {
+        initialize(this);
         telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double drive = -gamepad1.left_stick_y;
+            double strafe = gamepad1.left_stick_x;
+            double turn = gamepad1.right_stick_x;
+            double frontLeftPower = Range.clip(drive + strafe + turn, -1.0, 1.0);
+            double frontRightPower = Range.clip(drive - strafe + turn, -1.0, 1.0);
+            double backLeftPower = Range.clip(drive - strafe - turn, -1.0, 1.0);
+            double backRightPower = Range.clip(drive + strafe - turn, -1.0, 1.0);
+
+            frontLeft.setPower(frontLeftPower);
+            frontRight.setPower(frontRightPower);
+            backLeft.setPower(backLeftPower);
+            backRight.setPower(backRightPower);
+        }
     }
 }
