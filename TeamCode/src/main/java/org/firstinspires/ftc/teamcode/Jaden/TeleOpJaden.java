cd78c7f0d69491594145aa739b61d9beaf1cf72f
pi-person314 <jadenmengl@gmail.com>
2023-04-02 15:18:47 -0400

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Jaden/TeleOpJaden.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Jaden/TeleOpJaden.java
@@ -1,4 +1,16 @@
 package org.firstinspires.ftc.teamcode.Jaden;
 
-public class TeleOpJaden {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class TeleOpJaden extends LinearOpMode {
+
+    private DcMotor leftMotor;
+    private DcMotor rightMotor;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initialized");
+    }
 }
