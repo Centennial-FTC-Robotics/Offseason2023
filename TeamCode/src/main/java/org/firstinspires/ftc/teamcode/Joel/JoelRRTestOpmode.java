15667c3356319a5aa62d498e91cc6ea2ba36e83f
teky1 <joelchem@outlook.com>
2023-08-20 16:18:28 -0400

    laskdjfkldf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Joel/JoelRRTestOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Joel/JoelRRTestOpmode.java
@@ -3,12 +3,14 @@ package org.firstinspires.ftc.teamcode.Joel;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
-@TeleOp
+@Autonomous
 public class JoelRRTestOpmode extends LinearOpMode {
 
     public void runOpMode() {
@@ -18,17 +20,17 @@ public class JoelRRTestOpmode extends LinearOpMode {
 
         drive.setPoseEstimate(startPose);
 
-        Trajectory traj = drive.trajectoryBuilder(startPose)
-                .splineTo(new Vector2d(0, 36), 0)
-                .splineTo(new Vector2d(36, 36), 0)
-                .splineTo(new Vector2d(36, 0), 0)
-                .splineTo(new Vector2d(0, 0), 0)
+        TrajectorySequence traj = drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+                .lineToSplineHeading(new Pose2d(48, 0, Math.toRadians(90)))
+                .lineToSplineHeading(new Pose2d(48, 48, Math.toRadians(180)))
+                .lineToSplineHeading(new Pose2d(0, 48, Math.toRadians(270)))
+                .lineToSplineHeading(new Pose2d(0, 0, 0))
                 .build();
 
 
         waitForStart();
 
-        drive.followTrajectory(traj);
+        drive.followTrajectorySequence(traj);
 
     }
 
